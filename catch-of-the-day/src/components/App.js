d638a2ecad97f7785a31bc74a67c475c4c4dfade
Dnyaneshwar Ade <idnyaneshwarade@gmail.com>
2021-08-11 21:38:51 +0530

    Added EditFishForm to inventory and parsed

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,13 +14,26 @@ class App extends React.Component{
 
 componentDidMount() {
     const { params } = this.props.match;
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef){
+
+      this.setState({ order: JSON.parse(localStorageRef)})
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes"
     });
   };
 
-  componentWillUnmount() {
+  componentDidUpdate(){ 
+    console.log(this.state.order);
+    localStorage.setItem(
+      this.props.match.params.storeId, 
+      JSON.stringify(this.state.order)
+    );
+
+  }
+ componentWillUnmount() {
     base.removeBinding(this.ref);
   }
 
@@ -63,6 +76,7 @@ componentDidMount() {
                <Inventory 
                addFish={this.addFish} 
                loadSampleFishes={this.loadSampleFishes}
+               fishes={this.state.fishes}   
               />
             
             </div>
